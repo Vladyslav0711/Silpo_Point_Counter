257e04736affc61cc3e0055bd3e58d11580c1174
InterLink Intern <intern@intern-pc.interlink.office>
2019-11-06 15:10:40 +0200

    add use offer befor closed check

---
+++ src/checkout/CheckoutService.java
@@ -0,0 +1,43 @@
+package checkout;
+
+import java.util.*;
+
+public class CheckoutService {
+
+    private Check check;
+    private List<Offer> offers = new ArrayList<>();
+
+    public void addOffer(Offer offer){
+        offers.add(offer);
+    }
+    public void deleteOffer(Offer offer){
+        offers.remove(offer);
+    }
+    public void openCheck() {
+        check = new Check();
+    }
+
+    public void addProduct(Product product) {
+        if (check == null) {
+            openCheck();
+        }
+        check.addProduct(product);
+    }
+
+    public Check closeCheck() {
+        Check closedCheck = check;
+        for(Offer offer : offers){
+            useOffer(offer);
+           // deleteOffer(offer);
+        }
+        check = null;
+        return closedCheck;
+    }
+
+    private void useOffer(Offer offer) {
+        if (check != null)
+            offer.apply(check);
+    }
+}
+
+
